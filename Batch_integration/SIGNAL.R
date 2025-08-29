dace940951fd3aed1ee2fe3cd161606dd3d60ce3
Sheng Qiongyu <73150883+qiongyusheng@users.noreply.github.com>
2025-08-29 09:47:09 +0800

    Add files via upload

---
+++ Batch_integration/SIGNAL.R
@@ -0,0 +1,20 @@
+library(RSpectra)
+source("/home/server/zy/group_scripts/all_methods/utils.R")
+library(SIGNAL)
+
+ct_label = "CellType"
+
+setwd('/home/server/sqy/data/mosaic/input/human_mouse_WAT')
+meta <- readRDS('./meta.rds')
+human_homo <- as(as.matrix(readRDS('./human/rna_homo.rds')),'dgCMatrix')
+mouse_homo <- as(as.matrix(readRDS('./mouse/rna_homo.rds')),'dgCMatrix')
+X <- cbind(human_homo,mouse_homo)
+
+X <- Seurat::NormalizeData(X)
+res_SIGNAL = Run.gcPCA(X, meta, g_factor = "CellType", b_factor = "Batch", 
+                       npcs = 30, lambda = 50,do.scale = F,do.cosine = T)
+
+out_dir <- '...'
+write.csv(as.matrix(t(res_SIGNAL)),
+          paste0(out_dir,'SIGNAL.csv'),
+          row.names = FALSE)
